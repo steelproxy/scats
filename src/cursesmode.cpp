1dacf4d772b8b1dd3b3eeaa15f34e1dc078ff399
Collin Rodes <cr271620@ohio.edu>
2021-01-05 07:45:02 +0000

    finally working line history.

--- src/cursesmode.cpp
+++ src/cursesmode.cpp
@@ -17,6 +17,8 @@ void ngetstr(string &var)
     int var_buf = 0;
     size_t pos = 0;
     size_t lastCommandIndex = lastCommand.size() - 1;
+    bool historyScroll = false;
+    bool historyScrollDown = false;
 
     getyx(root, currentY, startingX);
 
@@ -28,7 +30,7 @@ void ngetstr(string &var)
 
         if (isprint(var_buf))
         {
-            insch(var_buf); // insert character left of current cursor pos
+            insch(var_buf);               // insert character left of current cursor pos
             move(currentY, currentX + 1); // move cursor forward one
             pos++;
         }
@@ -58,41 +60,84 @@ void ngetstr(string &var)
             move(currentY, currentX + 1);
             continue;
         }
-        else if(var_buf == KEY_UP)
+        else if (var_buf == KEY_UP)
         {
+            historyScrollDown = false;
             if(lastCommand.empty())
                 continue;
             var.clear();
             move(currentY, startingX);
             clrtoeol();
-            var = lastCommand.at(lastCommandIndex);
+            if(historyScroll == false)
+            {
+                var = lastCommand.at(lastCommandIndex);
+                historyScroll = true;
+            }
+            else if(lastCommandIndex != 0)
+                var = lastCommand.at(--lastCommandIndex);
+            else
+                var = lastCommand.at(lastCommandIndex);
             addstr(var.c_str());
             move(currentY, startingX + var.length());
             pos = var.length();
-            if(lastCommandIndex > 0 && !lastCommand.empty())
-                lastCommandIndex--;
             continue;
         }
-        else if(var_buf == KEY_DOWN)
+        else if (var_buf == KEY_DOWN)
         {
-            if(lastCommand.empty())
+            historyScroll = false;
+            if (lastCommand.empty())
                 continue;
+            if(lastCommandIndex == lastCommand.size())
+            {
+                var.clear();
+                move(currentY, startingX);
+                clrtoeol();
+                var = "";
+                addstr(var.c_str());
+                move(currentY, startingX + var.length());
+                pos = var.length();
+                continue;
+            }
             var.clear();
             move(currentY, startingX);
             clrtoeol();
-            var = lastCommand.at(lastCommandIndex);
+            
+            if(lastCommandIndex == lastCommand.size() - 1 && historyScrollDown == false)
+            {
+                var = lastCommand.at(lastCommandIndex);
+                historyScrollDown = true;
+            }
+            else if(lastCommandIndex == lastCommand.size() - 1)
+                var = "";
+            else
+                var = lastCommand.at(lastCommandIndex++);
             addstr(var.c_str());
             move(currentY, startingX + var.length());
             pos = var.length();
-            if(lastCommandIndex < lastCommand.size() - 1 && !lastCommand.empty())
-                lastCommandIndex++;
             continue;
         }
-        quickLog(VERBOSE, "startingx=" << startingX << " pos=" << pos << " currentx=" << currentX << " char='" << (char) var_buf << "'");
-        
-        if(var_buf != '\n')
-            var.insert(var.begin() + (pos-1), var_buf);
-    } 
+        else if (var_buf == '\t' && !var.empty())
+        {
+            for (string command : lastCommand)
+            {
+                if (command.find(var, 0) != string::npos)
+                {
+                    var.clear();
+                    move(currentY, startingX);
+                    clrtoeol();
+                    var = command;
+                    addstr(var.c_str());
+                    move(currentY, startingX + var.length());
+                    pos = var.length();
+                }
+            }
+            continue;
+        }
+        quickLog(VERBOSE, "startingx=" << startingX << " pos=" << pos << " currentx=" << currentX << " char='" << (char)var_buf << "'");
+
+        if (var_buf != '\n')
+            var.insert(var.begin() + (pos - 1), var_buf);
+    }
 
     move(currentY, startingX + var.length());
     addch('\n');
